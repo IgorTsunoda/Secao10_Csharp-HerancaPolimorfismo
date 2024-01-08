2fce2945bd05c5becd7651b1717a7878b6d304b0
Igor Tsunoda <igortaikitsunoda@gmail.com>
2024-01-07 21:01:35 -0300

    Iniciando repositorio.

---
+++ HerancaPolimormismo/HerancaPolimormismo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HerancaPolimormismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando aprendizado de herança e polimorfismo");
+        }
+    }
+}
