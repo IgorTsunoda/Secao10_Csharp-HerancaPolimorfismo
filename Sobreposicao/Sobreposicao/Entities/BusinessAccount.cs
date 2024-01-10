7dfa37c2a9b0e75e1b8e99e9aaaafc85944d1750
Igor Tsunoda <igortaikitsunoda@gmail.com>
2024-01-10 14:27:32 -0300

    Sobreposição, palavra virtual, override e base.

---
+++ Sobreposicao/Sobreposicao/Entities/BusinessAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobreposicao.Entities
+{
+   class BusinessAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public BusinessAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    
+        public override void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
