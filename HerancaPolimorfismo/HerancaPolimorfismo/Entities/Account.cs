a305f08b61df79c0c6ee875fc5062125e5f5c73d
Igor Tsunoda <igortaikitsunoda@gmail.com>
2024-01-10 10:24:19 -0300

    Adicionando Demo Herança.

---
+++ HerancaPolimorfismo/HerancaPolimorfismo/Entities/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+
+namespace HerancaPolimorfismo.Entities
+{
+     class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+    }
+}
---
+++ HerancaPolimorfismo/HerancaPolimorfismo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HerancaPolimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
