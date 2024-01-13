5c59e9ca23d324789bd79f829cf752dc85f44187
Igor Tsunoda <igortaikitsunoda@gmail.com>
2024-01-13 17:53:33 -0300

    Desafio herança e polimorfismo finalizado.

---
+++ Desafio/Desafio/Entities/ImportedProduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Desafio.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+        public double totalPrice()
+        {
+            return Price + CustomsFee;
+        }
+        public override string priceTag()
+        {
+            return Name
+                + " $ "
+                + totalPrice().ToString("F2", CultureInfo.InvariantCulture)
+                + " (Customs fee: $ "
+                + CustomsFee.ToString("F2", CultureInfo.InvariantCulture)
+                + ")";
+        }
+    }
+}
---
+++ Desafio/Desafio/Program.cs
@@ -0,0 +1,50 @@
+﻿using Desafio.Entities;
+using System.Globalization;
+
+namespace Desafio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> plist = new List<Product>();
+            Console.Write("Enter the number of products: ");
+            int qtdProducts = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= qtdProducts; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Comum, used or imported (c/u/i)? ");
+                char PrdClassification = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if(PrdClassification == 'i')
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    plist.Add(new ImportedProduct(name, price, customsFee));
+                }
+                else if (PrdClassification == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                    plist.Add(new UsedProduct(name, price, manufactureDate));
+                }
+                else if (PrdClassification == 'c')
+                {
+                    plist.Add(new Product(name, price));
+                }
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS: ");
+
+            foreach (Product p in plist)
+            {
+                Console.WriteLine(p.priceTag());
+            }
+        }
+    }
+}
